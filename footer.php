6b7c9ae530cc41d06688cc9dc4f32c46a694cfc5
Mark.K <kovacs.mark941007@gmail.com>
2026-05-17 17:25:13 +0200

    Update footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+        <div class="footer-container">
+            <p>&copy; <?php echo date('Y'); ?> Notebook Webshop. Minden jog fenntartva.</p>
+            <p>Készítették: Csoporttag 1 & Csoporttag 2 (Neumann János Egyetem)</p>
+        </div>
+    </footer>
+</body>
+</html>
