dded2225ee921639395f0f5250de3331e9189ff3
Mark.K <kovacs.mark941007@gmail.com>
2026-05-17 18:21:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <?php
+// index.php
 session_start();
+
+require_once 'Database/db.php';
 
 $oldal = isset($_GET['oldal']) ? $_GET['oldal'] : 'fooldal';
 
 if (!preg_match('/^[a-zA-Z0-9_-]+$/', $oldal)) {
+    $oldal = 'fooldal';
+}
+
+if ($oldal == 'kijelentkezes') {
+    session_unset();
+    session_destroy();
+    header("Location: index.php?oldal=fooldal");
+    exit();
+}
+
+if ($oldal == 'uzenetek' && !isset($_SESSION['user_id'])) {
     $oldal = 'fooldal';
 }
 
